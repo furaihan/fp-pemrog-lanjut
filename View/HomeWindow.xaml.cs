9505830e02e7adcf6adb4b08f35409166dfcffd1
TifaniiDea <tifanidea@students.amikom.ac.id>
2022-11-23 15:18:04 +0700

    commit home

---
+++ View/HomeWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PinusPengger.View
+{
+    /// <summary>
+    /// Interaction logic for HomeWindow.xaml
+    /// </summary>
+    public partial class HomeWindow : Window
+    {
+        public HomeWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
