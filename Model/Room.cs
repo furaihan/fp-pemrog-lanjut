155db0d5768a1eac3e57305ad0f4d5e1d79a570a
furaihan <28698437+furaihan@users.noreply.github.com>
2022-11-23 09:06:08 +0700

    MainWindow navbar

---
+++ Model/Room.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PinusPengger.Model
+{
+    public class Room
+    {
+        public int RoomId { get; set; }
+    }
+}
