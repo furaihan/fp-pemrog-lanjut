1bba16cc2a56db12a257405d65fd679074511440
furaihan <28698437+furaihan@users.noreply.github.com>
2022-11-24 19:23:53 +0700

    kamu nanyeaaa

--- Model/Room.cs
+++ Model/Room.cs
@@ -6,6 +6,13 @@ using System.Threading.Tasks;
 
 namespace PinusPengger.Model
 {
+    [Flags]
+    public enum RoomFeature : uint
+    {
+        DoubleBed = 1 << 0,
+        SingleBed = 1 << 1,
+        Heater = 1 << 2,
+    }
     public class Room
     {
         public int RoomId { get; set; }
