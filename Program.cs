f91c2ef8c328c28e5deb97b694253007740323ab
furaihan <28698437+furaihan@users.noreply.github.com>
2022-12-03 17:51:16 +0700

    fiesta chicken nugget

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PinusPengger
+{
+    public class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            App.Main();
+        }
+    }
+}
