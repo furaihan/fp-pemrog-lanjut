e361ede4f4b8419ea97e7e5bd41f822366e741b6
pijar <pijarpahlawanq@students.ac.id>
2022-12-10 13:00:52 +0700

    login view model

--- ViewModel/LoginViewModel.cs
+++ ViewModel/LoginViewModel.cs
@@ -1,45 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using PinusPengger.Records;
 using System.Windows.Input;
-using PinusPengger.Records;
 
 namespace PinusPengger.ViewModel
 {
     internal class LoginViewModel : ViewModelBase
     {
-        private ICommand loginCommand;
-        private ICommand showPasswordCommand;
-        private UserRecord _entity;
+        #region Field
+        private ICommand _loginCommand;
+        private ICommand _showPasswordCommand;
+        #endregion
+
+        #region Properties
+        public UserRecord UserRecEntity { get; set; }
+        public ICommand LoginCommand
+        {
+            get
+            {
+                if (_loginCommand == null)
+                {
+                    _loginCommand = new ViewModelCommand(param => ExecuteLoginCommand(), param => CanExecuteLoginCommand());
+                }
+
+                return _loginCommand;
+            }
+        }
+        public ICommand ShowPasswordCommand
+        {
+            get => _showPasswordCommand;
+            set => _showPasswordCommand = value;
+        }
+        #endregion
 
         //Command
-        public ICommand LoginCommand 
-        { 
-            get => loginCommand; 
-            set => loginCommand = value; 
-        }
-        public ICommand ShowPasswordCommand 
-        { 
-            get => showPasswordCommand; 
-            set => showPasswordCommand = value;
-        }
 
         public LoginViewModel()
         {
-            _entity = new UserRecord();
+            UserRecEntity = new UserRecord();
         }
         private bool CanExecuteLoginCommand()
         {
-            UserRecord user = new UserRecord();
-            bool valid = !string.IsNullOrWhiteSpace(user.Username) &&
-                !string.IsNullOrWhiteSpace(user.Password) &&
-                user.Username.Length > 3 &&
-                user.Password.Length > 3;
+            bool valid = !string.IsNullOrWhiteSpace(UserRecEntity.Username) &&
+                !string.IsNullOrWhiteSpace(UserRecEntity.Password) &&
+                UserRecEntity.Username.Length > 3 &&
+                UserRecEntity.Password.Length > 3;
             if (!valid)
             {
-                user.ErrorMessage = "Invalid username or password";
+                UserRecEntity.ErrorMessage = "Invalid username or password";
             }
             return valid;
         }
