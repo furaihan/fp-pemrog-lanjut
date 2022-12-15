68bc7d2426aa27a06124b47f0aa03b6d93888b74
pijar <pijarpahlawanq@students.ac.id>
2022-12-15 08:28:56 +0700

    create entity for join

---
+++ ViewModel/Extension/HistoryJoined.cs
@@ -0,0 +1,39 @@
+﻿using PinusPengger.Model;
+
+namespace PinusPengger.ViewModel.Extension
+{
+    class HistoryJoined : ViewModelBase
+    {
+        private History _historyEntity;
+        private Customer _customerEntity;
+        private Room _roomEntity;
+
+        public History HistoryEntity
+        {
+            get => _historyEntity;
+            set
+            {
+                _historyEntity = value;
+                OnPropertyChanged();
+            }
+        }
+        public Customer CustomerEntity
+        {
+            get => _customerEntity;
+            set
+            {
+                _customerEntity = value;
+                OnPropertyChanged();
+            }
+        }
+        public Room RoomEntity
+        {
+            get => _roomEntity;
+            set
+            {
+                _roomEntity = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
