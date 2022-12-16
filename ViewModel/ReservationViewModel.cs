312dc426ce0c45cb7e22e412216523290434a5dd
furaihan <28698437+furaihan@users.noreply.github.com>
2022-12-16 08:09:52 +0700

    re-enable fody in debug mode

--- ViewModel/ReservationViewModel.cs
+++ ViewModel/ReservationViewModel.cs
@@ -1,12 +1,35 @@
-﻿using System;
+﻿using PinusPengger.Model;
+using PinusPengger.UserControls;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PinusPengger.ViewModel
 {
-    class ReservationViewModel
+    internal class ReservationViewModel : ViewModelBase
     {
+        private ObservableCollection<RegularRoom> _regularRoom;
+
+        public ObservableCollection<RegularRoom> RegularRoom
+        {
+            get => _regularRoom; 
+            set
+            {
+                _regularRoom = value;
+                OnPropertyChanged();
+            }
+        }
+        private void RegularRoomInit()
+        {
+            int jumlahLantai = 8;
+            List<int> junmlahKamar = new List<int>() { 8, 9, 6, 7, 8, 9, 6, 5 };
+            for (int i = 0; i < jumlahLantai; i++)
+            {
+
+            }
+        }
     }
 }
