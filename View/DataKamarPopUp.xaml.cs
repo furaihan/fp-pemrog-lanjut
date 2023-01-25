666d477958e28b389b4ef9dd08a8978a075a1f2d
furaihan <28698437+furaihan@users.noreply.github.com>
2023-01-25 12:44:35 +0700

    data kamar

--- View/DataKamarPopUp.xaml.cs
+++ View/DataKamarPopUp.xaml.cs
@@ -23,5 +23,10 @@ namespace PinusPengger.View
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
     }
 }
