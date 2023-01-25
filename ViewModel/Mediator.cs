f28bd2122d14600dede126a45079e1a398d95aa6
pijar <pijarpahlawanq@students.ac.id>
2023-01-25 16:38:01 +0700

    initialize mediator pattern

---
+++ ViewModel/Mediator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PinusPengger.ViewModel
+{
+    public static class Mediator
+    {
+        private static IDictionary<string, List<Action<object>>> _callbackList = new Dictionary<string, List<Action<object>>>();
+        public static void Register(string token, Action<object> callback)
+        {
+            if (!_callbackList.ContainsKey(token))
+            {
+                _callbackList[token] = new List<Action<object>>();
+            }
+            _callbackList[token].Add(callback);
+        }
+        public static void Unregister(string token, Action<object> callback)
+        {
+            if (_callbackList.ContainsKey(token))
+            {
+                _callbackList[token].Remove(callback);
+            }
+        }
+        public static void NotifyColleagues(string token, object args)
+        {
+            if (_callbackList.ContainsKey(token))
+            {
+                List<Action<object>> list = _callbackList[token];
+                foreach (var callback in list)
+                {
+                    callback(args);
+                }
+            }
+        }
+    }
+}
