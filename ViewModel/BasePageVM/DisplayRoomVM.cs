28f69c7ab80da5c43e1888be068bff6d7e591f5e
furaihan <28698437+furaihan@users.noreply.github.com>
2023-01-29 02:23:41 +0700

    add IsDetailKamarWindowOpen to prevent multiple window open

--- ViewModel/BasePageVM/DisplayRoomVM.cs
+++ ViewModel/BasePageVM/DisplayRoomVM.cs
@@ -23,10 +23,12 @@ namespace PinusPengger.ViewModel.BasePageVM
             SelectedOption = Tag.RoomType.REG;
             _roomPool = Enumerable.Empty<RoomWithFacilities>();
             _reservations = Enumerable.Empty<ReservationJoined>();
-            _roomButtonCommand = new ViewModelCommand(ExecuteRoomButtonCommand);
+            _roomButtonCommand = new ViewModelCommand(ExecuteRoomButtonCommand, CanExecuteRoomButtonCommand);
+            _isDetailKamarWindowOpen = false;
             Options = new List<Tag.RoomType>() { Tag.RoomType.REG, Tag.RoomType.VIP };
             RoomWithFacilitiesObservable = new ObservableCollection<RoomWithFacilitiesObservable>();
             PropertyChanged += OnSelectedOptionChanged;
+            Mediator.Register("IsDetailKamarWindowOpenChanged", UpdateIsDetailKamarWindowOpen);
             GetData();
             ProcessData();
         }
@@ -38,6 +40,7 @@ namespace PinusPengger.ViewModel.BasePageVM
         private IEnumerable<RoomWithFacilities> _roomPool;
         private IEnumerable<ReservationJoined> _reservations;
         private ICommand _roomButtonCommand;
+        private bool _isDetailKamarWindowOpen;
         #endregion
 
         #region Properties
@@ -58,6 +61,11 @@ namespace PinusPengger.ViewModel.BasePageVM
                 _selectedOption = value;
                 OnPropertyChanged();
             }
+        }
+        public bool IsDetailKamarWindowOpen 
+        { 
+            get => _isDetailKamarWindowOpen; 
+            set => _isDetailKamarWindowOpen = value; 
         }
         public List<Tag.RoomType> Options { get; set; }
         public ObservableCollection<RoomWithFacilitiesObservable> RoomWithFacilitiesObservable { get; set; }
@@ -81,8 +89,21 @@ namespace PinusPengger.ViewModel.BasePageVM
             {
                 var DetailRoomWindow = new DataKamarPopUp();
                 Mediator.NotifyColleagues("ShowDetailRoom", obj);
+                IsDetailKamarWindowOpen = true;
                 DetailRoomWindow.Show();
             }
+        }
+        private bool CanExecuteRoomButtonCommand(object obj)
+        {
+            return !_isDetailKamarWindowOpen;
+        }
+        private void UpdateIsDetailKamarWindowOpen(object obj)
+        {
+            if (obj is not bool)
+            {
+                return;
+            }
+            IsDetailKamarWindowOpen = (bool)obj;
         }
         public void OnSelectedOptionChanged(object? sender, PropertyChangedEventArgs e)
         {
--- ViewModel/PopUpVM/DataKamarPopUpVM.cs
+++ ViewModel/PopUpVM/DataKamarPopUpVM.cs
@@ -1,16 +1,21 @@
 ﻿using PinusPengger.ViewModel.ObservableCombinedModel;
+using System.Diagnostics;
+using System.Windows.Input;
 
 namespace PinusPengger.ViewModel.PopUpVM
 {
     public class DataKamarPopUpVM : ViewModelBase
     {
         private RoomWithFacilitiesObservable _roomDetails;
+        private ICommand _onClose;
 
         public DataKamarPopUpVM()
         {
             _roomDetails = new RoomWithFacilitiesObservable();
+            _onClose = new ViewModelCommand(ExecuteOnCloseCommand);
             Mediator.Register("ShowDetailRoom", ShowDetailRoomWindow);
         }
+        public ICommand OnClose { get => _onClose; set => _onClose = value; }
 
         public RoomWithFacilitiesObservable RoomDetails
         {
@@ -20,6 +25,11 @@ namespace PinusPengger.ViewModel.PopUpVM
                 _roomDetails = value;
                 OnPropertyChanged();
             }
+        }
+        private void ExecuteOnCloseCommand(object obj)
+        {
+            Mediator.NotifyColleagues("IsDetailKamarWindowOpenChanged", false);
+            Debug.WriteLine("Closing DataKamarPopUp");
         }
 
         private void ShowDetailRoomWindow(object obj)
